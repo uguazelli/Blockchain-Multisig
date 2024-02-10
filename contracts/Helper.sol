8489a34db2e56662fbf0e8f84a663b8c6e04f0d4
uguazelli <guazelli.ugo@gmail.com>
2024-02-10 16:55:35 -0300

    Creating a helper library

---
+++ contracts/Helper.sol
@@ -0,0 +1,67 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+library Helper{
+
+    function createIdentifier(address[] memory uniqueAddresses) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(uniqueAddresses));
+    }
+
+    function bytesInArray(bytes32[] memory array, bytes32 value) internal pure returns (bool) {
+        for (uint i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function addressInArray(address[] memory array, address value) internal pure returns (bool) {
+        for (uint i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function areArraysEqual(address[] memory arr1, address[] memory arr2) internal pure returns (bool) {
+        if(arr1.length != arr2.length) {
+            return false;
+        }
+
+        // Sort the arrays
+        quickSort(arr1, int(0), int(arr1.length - 1));
+        quickSort(arr2, int(0), int(arr2.length - 1));
+
+        // Compare the sorted arrays
+        for(uint i = 0; i < arr1.length; i++) {
+            if(arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function quickSort(address[] memory arr, int left, int right) internal pure {
+        int i = left;
+        int j = right;
+        if(i==j) return;
+        address pivot = arr[uint(left + (right - left) / 2)];
+        while (i <= j) {
+            while (arr[uint(i)] < pivot) i++;
+            while (pivot < arr[uint(j)]) j--;
+            if (i <= j) {
+                (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
+                i++;
+                j--;
+            }
+        }
+        if (left < j)
+            quickSort(arr, left, j);
+        if (i < right)
+            quickSort(arr, i, right);
+    }
+
+}
--- contracts/Marriage.sol
+++ contracts/Marriage.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
+import "hardhat/console.sol";
+import "./Helper.sol";
 
 contract Marriage {
 
@@ -13,12 +15,10 @@ contract Marriage {
     }
 
     Details d;
-    
 
     mapping(bytes32 => Details) private detailsById;
     mapping(address => bytes32[]) private idsByAddress;
 
-   
 
     function getDetails(bytes32 _id) public view returns (Details memory){
         return detailsById[_id];
@@ -31,8 +31,10 @@ contract Marriage {
 
     function createContract(bytes32 _id, address[] memory _addresses, string memory _agreement ) public  {
 
+
         if(_id == 0x0000000000000000000000000000000000000000000000000000000000000000){
-            _id = createIdentifier(_addresses);
+            console.log("Creating ID");
+            _id = Helper.createIdentifier(_addresses);
             d = detailsById[_id];
             d.addresses = _addresses;
             d.confirmed = [msg.sender];
@@ -41,89 +43,28 @@ contract Marriage {
             d.agreement = _agreement;
 
         }else{
+            console.log("Using existed ID");
             d = detailsById[_id];
 
-            if(areArraysEqual(d.addresses, d.confirmed)){
-                d.status = 1;
-            }
-
-            if(!addressInArray(d.confirmed, msg.sender)) {
+            if(!Helper.addressInArray(d.confirmed, msg.sender)) {
+                console.log("Addresses not in array");
                 d.confirmed.push(msg.sender);
             }   
+            
+            if(Helper.areArraysEqual(d.addresses, d.confirmed)){
+                console.log("Addresses matches");
+                d.status = 1;
+            }
                
         }
-            if(!bytesInArray(idsByAddress[msg.sender], _id)){
+            if(!Helper.bytesInArray(idsByAddress[msg.sender], _id)){
+                console.log("Bytes not in array");
                 idsByAddress[msg.sender].push(_id);  
             }   
 
-            detailsById[_id] = d;
-            
-        
-        
+            detailsById[_id] = d;    
         
     }
-
-    function createIdentifier(address[] memory uniqueAddresses) private pure returns (bytes32) {
-        return keccak256(abi.encodePacked(uniqueAddresses));
-    }
-
-    function bytesInArray(bytes32[] memory array, bytes32 value) private pure returns (bool) {
-        for (uint i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function addressInArray(address[] memory array, address value) private pure returns (bool) {
-        for (uint i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function areArraysEqual(address[] memory arr1, address[] memory arr2) private pure returns (bool) {
-        if(arr1.length != arr2.length) {
-            return false;
-        }
-
-        // Sort the arrays
-        quickSort(arr1, int(0), int(arr1.length - 1));
-        quickSort(arr2, int(0), int(arr2.length - 1));
-
-        // Compare the sorted arrays
-        for(uint i = 0; i < arr1.length; i++) {
-            if(arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    function quickSort(address[] memory arr, int left, int right) private pure {
-        int i = left;
-        int j = right;
-        if(i==j) return;
-        address pivot = arr[uint(left + (right - left) / 2)];
-        while (i <= j) {
-            while (arr[uint(i)] < pivot) i++;
-            while (pivot < arr[uint(j)]) j--;
-            if (i <= j) {
-                (arr[uint(i)], arr[uint(j)]) = (arr[uint(j)], arr[uint(i)]);
-                i++;
-                j--;
-            }
-        }
-        if (left < j)
-            quickSort(arr, left, j);
-        if (i < right)
-            quickSort(arr, i, right);
-    }
-
 
 
 }
