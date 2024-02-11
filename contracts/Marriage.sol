c4ca82647acea77b0b7064b04aba82e31a4e1a23
uguazelli <guazelli.ugo@gmail.com>
2024-02-10 21:14:32 -0300

    add a requiere to not modify after settle, Removed console.log

--- contracts/Marriage.sol
+++ contracts/Marriage.sol
@@ -29,11 +29,9 @@ contract Marriage {
     }
 
 
-    function createContract(bytes32 _id, address[] memory _addresses, string memory _agreement ) public  {
-
+    function createContract(bytes32 _id, address[] memory _addresses, string memory _agreement ) public {
 
         if(_id == 0x0000000000000000000000000000000000000000000000000000000000000000){
-            console.log("Creating ID");
             _id = Helper.createIdentifier(_addresses);
             d = detailsById[_id];
             d.addresses = _addresses;
@@ -43,27 +41,24 @@ contract Marriage {
             d.agreement = _agreement;
 
         }else{
-            console.log("Using existed ID");
             d = detailsById[_id];
 
+            require(d.status == 0, "Status 1 cannot be modified.");
+
             if(!Helper.addressInArray(d.confirmed, msg.sender)) {
-                console.log("Addresses not in array");
                 d.confirmed.push(msg.sender);
             }   
             
             if(Helper.areArraysEqual(d.addresses, d.confirmed)){
-                console.log("Addresses matches");
                 d.status = 1;
             }
                
         }
             if(!Helper.bytesInArray(idsByAddress[msg.sender], _id)){
-                console.log("Bytes not in array");
                 idsByAddress[msg.sender].push(_id);  
             }   
 
-            detailsById[_id] = d;    
-        
+            detailsById[_id] = d;        
     }
 
 
@@ -73,11 +68,11 @@ contract Marriage {
 // TEST DATA
 
 /*
- key 0x0000000000000000000000000000000000000000000000000000000000000000
- addr1 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
- aadr2 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
- status 1 (married)
- agreement MarriageTest
+ key - 0x0000000000000000000000000000000000000000000000000000000000000000
+ addr1 - 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+ aadr2 - 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+ status - 0 - Pending / 1 Confirmed
+ agreement - MarriageTest
 
 0x0000000000000000000000000000000000000000000000000000000000000000, [0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2], "MarriageTest"
  
